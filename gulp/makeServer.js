8b9d3448d1eaf5e6f7733a37f6256fda93d2fbd6
Dima Panov <rpsdpano@gmail.com>
2018-03-17 12:08:43 +0300

    Fix gulp makeServer task

--- gulp/makeServer.js
+++ gulp/makeServer.js
@@ -17,6 +17,7 @@ module.exports = function() {
                     {
                         "name": "[name].[ext]",
                         "extensions": ["png", "jpg", "jpeg", "gif"],
+                        "publicPath": "static/images/",
                         "emitFile": false
                     }
                 ]
